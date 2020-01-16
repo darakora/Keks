8872d0e35d01c064053fd1224e5d6a5a67058a00
Dasha <korobankoforstudy@gmail.com>
2020-01-16 03:10:35 +0300

    wtf

---
+++ SavaKursach/ViewModel/ProjectsListWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavaKursach.ViewModel
+{
+    public class ProjectsListWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Project> Projects
+        {
+            get
+            {
+                using (var context = new KursachDBContext())
+                {
+                    return new ObservableCollection<Project>(context.Projects);
+                }
+            }
+        }
+    }
+}
